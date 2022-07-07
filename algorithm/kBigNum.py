8b54c22c809da7fd9b7484e854f42f92a3ea9d16
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2022-07-08 01:28:01 +0900

    UPDATE: 대표값 문제 해결

--- algorithm/kBigNum.py
+++ algorithm/kBigNum.py
@@ -6,10 +6,7 @@ sys.stdin = open("input.txt", "rt")
 n, k = map(int, input().split())
 nums = list(map(int, input().split()))
 c_nums = list(combinations(nums, 3))
-# print(c_nums)
-sum_nums = list()
-for comb in c_nums:
-    sum_nums.append(sum(comb))
+sum_nums = [sum(comb) for comb in c_nums]
 
 sum_nums.sort(reverse=True)
 print(sum_nums[k - 1])
