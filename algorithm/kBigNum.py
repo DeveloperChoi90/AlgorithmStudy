9b7485a10a6bbc7a047183101d70edbbc5f02645
DeveloperChoi90 <DeveloperChoi90@gmail.com>
2022-07-09 11:44:18 +0900

    HOTFIX: K번째 큰 수, 경우의 수를 구해 합을 구했을 때 중복 수를 고려했어야 한다.

--- algorithm/kBigNum.py
+++ algorithm/kBigNum.py
@@ -6,7 +6,12 @@ sys.stdin = open("input.txt", "rt")
 n, k = map(int, input().split())
 nums = list(map(int, input().split()))
 c_nums = list(combinations(nums, 3))
-sum_nums = [sum(comb) for comb in c_nums]
 
-sum_nums.sort(reverse=True)
-print(sum_nums[k - 1])
+# 합을 구해서 중복수가 발생하는 것을 생각해야 한다.
+sum_nums = set()
+for comb in c_nums:
+    sum_nums.add(sum(comb))
+# for comb in c_nums:
+#   sum_nums.append(sum(comb))
+# sum_nums.sort(reverse=True)
+print(sorted(sum_nums, reverse=True)[k - 1])
